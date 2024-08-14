e74740facb8ce20a0e12c929ab60464ca4a9d838
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2024-08-14 18:22:23 +0700

    add basic login/signup code

--- B2D_Ventures/urls.py
+++ B2D_Ventures/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- B2D_Ventures/views.py
+++ B2D_Ventures/views.py
@@ -1,6 +1,44 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
 def index(request) -> HttpResponse:
     return render(request, "index.html")
+
+def register_view(request):
+    print("TESTETSTESTSET")
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.info(request, "Registration Successful!")
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+        messages.error(request, "Invalid username or password.")
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "Login Successful!")
+                return redirect('index')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
