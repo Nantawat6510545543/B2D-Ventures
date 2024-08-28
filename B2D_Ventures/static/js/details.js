90b7c8d5daacf991833ab7f811899c01fa448cce
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2024-08-28 23:44:17 +0700

    redo slider setup due to bug

--- B2D_Ventures/static/js/details.js
+++ B2D_Ventures/static/js/details.js
@@ -1,16 +1,28 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
+// $(document).ready(() => {
+//     $('.slider-for').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         fade: true,
+//         asNavFor: '.slider-nav'
+//     });
 
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
+//     $('.slider-nav').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         asNavFor: '.slider-for',
+//         dots: true,
+//         centerMode: true,
+//         focusOnSelect: true
+//     });
+// });
+
+$(document).ready(() => {
+    $('.slick-carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true
+    });
 });
